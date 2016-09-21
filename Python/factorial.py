337faccf9122ebdfad55d63cfecf118221bd9b1e
atorresunger <atorres.unger@gmail.com>
2016-09-21 10:53:02 -0500

    Update factorial.py

--- Python/factorial.py
+++ Python/factorial.py
@@ -1,7 +1,69 @@
 def factorial (numero): 
     print numero
     if numero==0:
-      return 1
-    else:
-      return numero+factorial(numero-2)
+        
+        
+        printf bla bla bla
+        
+        
+        
+        
+      r
+      e
+      t
+      u
+      r
+      n
+      1
+      
+    el
+    s
+    e
+    :
+        
+      
+      r
+      e
+      t
+      u
+      r
+      n
+      n
+      
+      u
+      m
+      
+      
+      
+      
+      e
+      r
+      o
+      
+      +
+      f
+      a
+      c
+      t
+      
+      o
+      r
+      
+      ia
+      
+      
+      
+      
+      l
+      (
+          n
+          u
+          m
+          
+          e
+          r
+          
+          o
+          -
+          2)
 factorial(12)
